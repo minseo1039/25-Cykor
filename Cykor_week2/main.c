ef0ba2a309769b671e5d7504c655a34caa169128
송민서 <minseo1039@naver.com>
2025-05-08 15:45:58 +0900

    cd pwd 구현

--- Cykor_week2/main.c
+++ Cykor_week2/main.c
@@ -22,7 +22,6 @@ void main_window() // 초기 화면을 출력하는 함수
     printf("Please enter the command you want to execute.\n");
     printf("If you want to exit, please enter 'exit'.\n");
     printf("===============================================\n");
-    printf("%s@%s:", pw->pw_name, hostname);
 }
 
 Node *create_node(char *folder) // 노드를 생성하는 함수
@@ -33,14 +32,20 @@ Node *create_node(char *folder) // 노드를 생성하는 함수
     return new_node;    
 }
 
+void free_path_list(Node *head) {
+    Node *current = head;
+    Node *next;
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
 Node *load_current_path()
 {
     char cwd[1024];
-    if(getcwd(cwd, sizeof(cwd)) == NULL)
-    {
-        perror("getcwd 실패");
-        exit(1);
-    }
+    getcwd(cwd, sizeof(cwd));
 
     Node *head = NULL;
     Node *tail = NULL;
@@ -77,7 +82,8 @@ void print_path(Node *head) {
 int main(void)
 {
     uid_t uid = getuid();
-    int i;
+    int i, cnt=0;
+    char command[256];
     Node *path_list = load_current_path();
 
     pw = getpwuid(uid);
@@ -85,8 +91,47 @@ int main(void)
 
     main_window();
 
-    print_path(path_list);
-    printf("$");
-    printf("\n");
+    while(1)
+    {
+        char *args[20];
+        char *ptr;
+        cnt = 0;
+        printf("%s@%s:", pw->pw_name, hostname);
+        print_path(path_list);
+        printf("$");
+        fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\n")] = 0; // 개행 문자 제거
+        args[0] = strtok(command, " ");
+        while(args[cnt] != NULL)
+        {
+            args[++cnt] = strtok(NULL, " ");
+        }
+        if(strcmp(args[0], "exit") == 0) // exit 입력 시 종료
+        {
+            printf("Exit the shell.\n");
+            break;
+        }
+        else if(strcmp(args[0], "pwd") == 0) // pwd 입력 시 현재 경로 출력
+        {
+            print_path(path_list);
+            printf("\n");
+        }
+        else if(strcmp(args[0], "cd") == 0) // cd 입력 시 경로 변경
+        {
+            if(chdir(args[1]) == -1) // 경로 변경 실패 시 오류 메시지 출력
+            {
+                perror("cd failed");
+            }
+            else // 경로 변경 성공 시 경로 리스트 업데이트
+            {
+                free_path_list(path_list); // 기존 경로 리스트 메모리 해제
+                path_list = load_current_path(); // 새로운 경로 리스트 로드
+            }
+        }
+        else // 잘못된 명령어 입력 시 오류 메시지 출력
+        {
+            printf("Invalid command. Please try again.\n");
+        }
+    }
     return 0;
 }
