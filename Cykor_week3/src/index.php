27333af2b9d2154e761f9dc40159bb79811a1e59
송민서 <minseo1039@naver.com>
2025-05-29 19:46:16 +0900

    Main, Login, Register 페이지 간단하게 생성

--- Cykor_week3/src/index.php
+++ Cykor_week3/src/index.php
@@ -1,3 +1,16 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Cykor Week 3</title>
+</head>
+<body>
+    <h1>Main Page</h1>
+    <h2>Welcome to Song Min Seo's Cykor Week 3</h2>
+    <ul>
+        <li><a href = "./index.php">Main</a></li>
+        <li><a href = "./login.php">Login</a></li>
+        <li><a href = "./register.php">Register</a></li>
+    </ul>
+</body>
+</html>
---
+++ Cykor_week3/src/register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Cykor Week 3</title>
+</head>
+<body>
+    <h1>Register Page</h1>
+    <ul>
+        <li><a href = "./index.php">Main</a></li>
+        <li><a href = "./login.php">Login</a></li>
+        <li><a href = "./register.php">Register</a></li>
+    </ul>
+</body>
+</html>
